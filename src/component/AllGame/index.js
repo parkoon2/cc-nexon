43f592606a1ff5b55ffb4c1e24795609d65bc9e6
parkoon <par_koon@naver.com>
2019-09-26 13:59:59 +0900

    all games completed

--- src/component/AllGame/index.js
+++ src/component/AllGame/index.js
@@ -1,71 +1,125 @@
-import React from "react";
+import React, { createRef } from "react";
 import "./index.css";
 import Card from "../Card";
-const AllGames = () => (
-  <div className="all-games">
-    <div className="header">
-      <div className="title">
-        <h2>전체게임</h2>
-        <div className="view-type">
-          <input type="radio" id="thumbnail" name="view-type" checked></input>
-          <span>
-            <label for="thumbnail">
-              <i class="fas fa-th-large"></i>
-            </label>
-          </span>
-          <input type="radio" id="list" name="view-type"></input>
-          <span>
-            <label for="list">
-              <i class="fas fa-list"></i>
-            </label>
-          </span>
+import MobileCard from "../MobileCard";
+class AllGames extends React.Component {
+  mobileRef = createRef();
+  pcRef = createRef();
+
+  state = {
+    currentView: "thumbnail" // thunmbnail or list
+  };
+
+  scrollToMobile = () => {
+    this.mobileRef.current.scrollIntoView({
+      behavior: "smooth"
+    });
+  };
+
+  handleViewChange = e => {
+    this.setState({
+      currentView: e.target.id
+    });
+  };
+
+  render() {
+    const { currentView } = this.state;
+
+    return (
+      <div className="all-games">
+        <div className="header">
+          <div className="title">
+            <h2>전체게임</h2>
+            <div className="view-type">
+              <input
+                type="radio"
+                id="thumbnail"
+                name="view-type"
+                checked={currentView === "thumbnail"}
+                onChange={this.handleViewChange}
+              ></input>
+              <span>
+                <label for="thumbnail">
+                  <i class="fas fa-th-large"></i>
+                </label>
+              </span>
+
+              <input
+                type="radio"
+                id="list"
+                name="view-type"
+                checked={currentView === "list"}
+                onChange={this.handleViewChange}
+              ></input>
+              <span>
+                <label for="list">
+                  <i class="fas fa-list"></i>
+                </label>
+              </span>
+            </div>
+          </div>
+          <div className="search-bar">
+            <input placeholder="게임명 검색"></input>
+            <i class="fas fa-search"></i>
+          </div>
         </div>
+        <div className="filter">
+          <a href="/" className="selected">
+            #전체
+          </a>
+          <a href="/">#최근 게이머가 많은 TOP10</a>
+          <a href="/">#오래 플레이하는 TOP10</a>
+          <a href="/">#RPG</a>
+          <a href="/">#액션 RPG</a>
+          <a href="/">#AOS/MOBA</a>
+          <a href="/">#FPS</a>
+          <a href="/">#캐주얼</a>
+          <a href="/">#스포츠</a>
+          <a href="/">#이벤트</a>
+          <a href="/">#업데이트</a>
+          <a href="/">#신규게임</a>
+        </div>
+        <div className="pc">
+          <div className="type">
+            <h2 ref={this.pcRef}>PC게임</h2>
+            <h2 className="type-mobile" onClick={this.scrollToMobile}>
+              모바일게임
+            </h2>
+          </div>
+          <div className="pc-games">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </div>
+        </div>
+        <div className="mobile" ref={this.mobileRef}>
+          <h2 className="type">모바일게임</h2>
+          <div className="mobile-games">
+            <MobileCard />
+            <MobileCard />
+            <MobileCard />
+            <MobileCard />
+            <MobileCard />
+            <MobileCard />
+            <MobileCard />
+            <MobileCard />
+            <MobileCard />
+          </div>
+        </div>
+        <div></div>
       </div>
-      <div className="search-bar">
-        <input placeholder="게임명 검색"></input>
-        <i class="fas fa-search"></i>
-      </div>
-    </div>
-    <div className="filter">
-      <a href="/" className="selected">
-        #전체
-      </a>
-      <a href="/">#최근 게이머가 많은 TOP10</a>
-      <a href="/">#오래 플레이하는 TOP10</a>
-      <a href="/">#RPG</a>
-      <a href="/">#액션 RPG</a>
-      <a href="/">#AOS/MOBA</a>
-      <a href="/">#FPS</a>
-      <a href="/">#캐주얼</a>
-      <a href="/">#스포츠</a>
-      <a href="/">#이벤트</a>
-      <a href="/">#업데이트</a>
-      <a href="/">#신규게임</a>
-    </div>
-    <div className="pc">
-      <div className="type">
-        <h2>PC게임</h2>
-        <h2 className="mobile">모바일게임</h2>
-      </div>
-      <div className="card-container">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
-    </div>
-    <div></div>
-    <div></div>
-  </div>
-);
+    );
+  }
+}
 
 export default AllGames;
---
+++ src/component/MobileCard/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./index.css";
+
+const MobileCard = () => {
+  return (
+    <div className="mobile-card">
+      <img src={require("../../images/mobile-card.png")} alt="game-image" />
+      <div className="info">
+        <div className="title">
+          <h2>
+            V4 <span className="event">EVENT</span>
+          </h2>
+          <div className="genre">MMORPG</div>
+        </div>
+        <div className="shop">
+          <i className="fab fa-android android"></i>
+          <i className="fab fa-apple ios"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MobileCard;
