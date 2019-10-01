276c337700401f15d377bf6a872758bb5c9d5bf7
parkoon <par_koon@naver.com>
2019-10-01 13:58:14 +0900

    Updates

---
+++ precache-manifest.c7395c8374ea54631074e92861d8fd4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db318c2aade94594eed5db64581ea182",
+    "url": "/cc-nexon/index.html"
+  },
+  {
+    "revision": "66874470b5a8ef3596b0",
+    "url": "/cc-nexon/static/css/main.484ec243.chunk.css"
+  },
+  {
+    "revision": "7b702bcaa8b8938bb835",
+    "url": "/cc-nexon/static/js/2.c22d2b4c.chunk.js"
+  },
+  {
+    "revision": "66874470b5a8ef3596b0",
+    "url": "/cc-nexon/static/js/main.e6cadef5.chunk.js"
+  },
+  {
+    "revision": "15afbc18bb8169502ea3",
+    "url": "/cc-nexon/static/js/runtime-main.9ecd5d14.js"
+  },
+  {
+    "revision": "0483276f20f1103cbd92598246f95c0f",
+    "url": "/cc-nexon/static/media/mobile-card.0483276f.png"
+  },
+  {
+    "revision": "9f42935eccf96080d0a16f630e809e14",
+    "url": "/cc-nexon/static/media/sample-card.9f42935e.jpg"
+  }
+]);
